07273259ea0b92f1ad1d6c5eac9d60f8b66950f5
Alexander Hoyos <urrao100@gmail.com>
2021-07-15 19:36:03 -0500

    adding initial page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,9 @@
 const Header = () => {
-  return <h5 className="card-header text-center"  >GIT HUB REPORT</h5>;
-  
-<div>
-<div className="glyphicon glyphicon-home"></div>
-<div className="material-icons-search"></div>
-<div className="material-icons-cloud<"></div>
-<div className="material-icons-delete"></div>
-  </div>
+  return (
+    <div>
+      <h5 className="card-header text-center">GIT HUB REPORT</h5>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/searchBox/SearchBox.js
+++ src/components/searchBox/SearchBox.js
@@ -1,14 +1,17 @@
+import "./searchBox.css";
 const SearchBox = () => {
   return (
-    <div>
-      <label>
-        {"User"}
-        <input type="text" placeholder="usuario" ></input>
-      </label>
-      <label>
-        {"Search"}
-        <input type="search"></input>
-      </label>
+    <div className = "big-square">
+      <div className="square">
+        <div className="text-center" id="border">
+          <input type="text" placeholder="ingrese usuario"></input>
+        </div>
+        <div className="text-center" id="button">
+          <button type="button" className="btn btn-success">
+            Buscar
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
